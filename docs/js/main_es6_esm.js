f58aaabc0b29d7afd4b28323d09a93b8b7b35269
Luca Zampetti <lzampetti@gmail.com>
2020-03-11 17:50:37 +0100

    Updating - Bundler

--- docs/js/main_es6_esm.js
+++ docs/js/main_es6_esm.js
@@ -182,13 +182,16 @@ class Main {
   	console.log('Main.addSimpleComponents', instances);
   	return instances;
   }
-  	addTemplateComponents() {
+  
+  addTemplateComponents() {
   	const instances = Array.from(document.querySelectorAll('[template-component]'))
   		.map(node => new TemplateComponent().setNode(node));
-  		console.log('Main.addTemplateComponents', instances);
+  
+  	console.log('Main.addTemplateComponents', instances);
   	return instances;
   }
-  	addButtonComponents() {
+  
+  addButtonComponents() {
   	const instances = Array.from(document.querySelectorAll('[button-component]'))
   		.map(node => {
   			const instance = new ButtonComponent().setNode(node);
@@ -197,7 +200,8 @@ class Main {
   			});
   			return instance;
   		});
-  		console.log('Main.addButtonComponents', instances);
+  
+  	console.log('Main.addButtonComponents', instances);
   	return instances;
   }
   */
